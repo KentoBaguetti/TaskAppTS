36444a7271b2f062a9b27a6ad2e571c78ed2c0c3
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-06-14 16:34:51 -0400

    TS config stuff

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,3 +1,7 @@
+import "../styles/home.css";
+import axios from "axios";
+import { useState } from "react";
+
 const Home = () => {
   return <div className="home"></div>;
 };
---
+++ server/dist/database/tempDatabase.js
@@ -0,0 +1,41 @@
+"use strict";
+const db = {
+    users: {
+        messi: {
+            name: "Lionel Messi",
+            nationality: "Argentina",
+            number: 10,
+            goldenBalls: 8,
+        },
+        cr7: {
+            name: "Cristiano Ronaldo",
+            nationality: "Portugal",
+            number: 7,
+            goldenBalls: 5,
+        },
+        honda: {
+            name: "Keisuke Honda",
+            nationality: "Japan",
+            number: 4,
+            goldenBalls: 0,
+        },
+    },
+    leagues: {
+        spain: {
+            name: "LaLiga",
+        },
+        england: {
+            name: "English Premiere League",
+        },
+        germany: {
+            name: "Bundesliga",
+        },
+        saudi: {
+            name: "Saudi Pro League",
+        },
+    },
+};
+const getUsers = () => {
+    return db.users;
+};
+console.log(getUsers());
